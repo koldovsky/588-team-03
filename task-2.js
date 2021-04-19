941db3b75ae86622000f80834265734d75cc90a5
Luciferskiy <81638824+Luciferskiy@users.noreply.github.com>
2021-04-19 15:55:38 +0300

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -3,3 +3,19 @@ function stringToArray(string){
 }
 
 //задача 1 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+//задача 2 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+const min = list => {
+    
+    return Math.min(...list);
+}
+
+const max = list => {
+    
+    return Math.max(...list);
+}
+//задача 3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
