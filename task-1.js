23ad8d97c936833c554d537e02b76b0e31907576
Luciferskiy <81638824+Luciferskiy@users.noreply.github.com>
2021-04-19 16:24:03 +0300

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -3,28 +3,28 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return totalGoals;
 }
 
-//завдання 1
+//завдання 1 https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 function move(position, roll) {
   const newPosition = position + roll * 2;;
   return newPosition;
 }
 
-//завдання 2
+//завдання 2 https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
   
 function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
 
-//завдання 3
+//завдання 3 https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
   
 function litres(time) {
   return Math.floor(time / 2);
 }
 
-//завдання 4
+//завдання 4 https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
   
 function mango(quantity, price) {
@@ -32,4 +32,4 @@ function mango(quantity, price) {
   return (quantity - freeMangos) * price;
 }
 
-//завдання 5
+//завдання 5 https://www.codewars.com/kata/57a77726bb9944d000000b06
