1b4c8f3ccd5d273ef33bd114201876f23a321929
Luciferskiy <81638824+Luciferskiy@users.noreply.github.com>
2021-04-21 14:13:48 +0300

    Create task03.js

---
+++ task03.js
@@ -0,0 +1,8 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+//завдання 1  https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
