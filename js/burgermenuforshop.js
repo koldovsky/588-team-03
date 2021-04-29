32c7e0e4ed1d6dad63804ae63ddb270b50f45c50
Vladislava Frantsishkevych <vladkafranse@gmail.com>
2021-04-29 22:42:27 +0300

    add basic cart implementation

---
+++ js/burgermenuforshop.js
@@ -0,0 +1,46 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+       return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry:function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+     }, 
+     iOS:function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+     }, 
+     Opera:function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+     }, 
+     Windows:function () {
+        return navigator.userAgent.match(/IEMobile/i);
+     }, 
+     any: function(){
+         return(
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+     }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');  
+} else {
+    document.body.classList.add('pc'); 
+}
+
+
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu){
+    const menu = document.querySelector('.menu');
+    iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menu.classList.toggle('_active');
+    });
+
+    
+}
